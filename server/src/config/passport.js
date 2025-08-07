da304d8490177a2b4637b721a22448ac8644eaf6
Utkarsharma7 <utkarsh22sharma1@gmail.com>
2025-08-07 07:08:23 +0530

    made some change to the server i forgot what i did

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../model/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "http://localhost:3000/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
